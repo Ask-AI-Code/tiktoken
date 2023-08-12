b4649797586a8bb4d7b6ab6d2836d8549e8bee24
Bar Admoni <bar@ask-ai.com>
2023-08-12 10:51:56 +0300

    Add typing-extensions

--- tiktoken/core.py
+++ tiktoken/core.py
@@ -2,7 +2,8 @@ from __future__ import annotations
 
 import functools
 from concurrent.futures import ThreadPoolExecutor
-from typing import AbstractSet, Collection, Literal, NoReturn, Optional, Union
+from typing import AbstractSet, Collection, NoReturn, Optional, Union
+from typing_extensions import Literal
 
 import regex
 
@@ -129,7 +130,9 @@ class Encoding:
             text = text.encode("utf-16", "surrogatepass").decode("utf-16", "replace")
             return self._core_bpe.encode(text, allowed_special)
 
-    def encode_ordinary_batch(self, text: list[str], *, num_threads: int = 8) -> list[list[int]]:
+    def encode_ordinary_batch(
+        self, text: list[str], *, num_threads: int = 8
+    ) -> list[list[int]]:
         """Encodes a list of strings into tokens, in parallel, ignoring special tokens.
 
         This is equivalent to `encode_batch(text, disallowed_special=())` (but slightly faster).
@@ -168,7 +171,9 @@ class Encoding:
             disallowed_special = frozenset(disallowed_special)
 
         encoder = functools.partial(
-            self.encode, allowed_special=allowed_special, disallowed_special=disallowed_special
+            self.encode,
+            allowed_special=allowed_special,
+            disallowed_special=disallowed_special,
         )
         with ThreadPoolExecutor(num_threads) as e:
             return list(e.map(encoder, text))
@@ -311,7 +316,9 @@ class Encoding:
         with ThreadPoolExecutor(num_threads) as e:
             return list(e.map(decoder, batch))
 
-    def decode_bytes_batch(self, batch: list[list[int]], *, num_threads: int = 8) -> list[bytes]:
+    def decode_bytes_batch(
+        self, batch: list[list[int]], *, num_threads: int = 8
+    ) -> list[bytes]:
         """Decodes a batch (list of lists of tokens) into a list of bytes."""
         with ThreadPoolExecutor(num_threads) as e:
             return list(e.map(self.decode_bytes, batch))
@@ -365,7 +372,6 @@ class Encoding:
 
     def _encode_bytes(self, text: bytes) -> list[int]:
         return self._core_bpe._encode_bytes(text)
-
 
 
 @functools.lru_cache(maxsize=128)
